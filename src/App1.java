6ef7a4d2141a17006e13cdc7a34f861c33e83ca7
sakthivel <sakthivel90bca@gmail.com>
2019-01-10 15:57:31 +0530

    Change in test

--- src/App1.java
+++ src/App1.java
@@ -3,9 +3,15 @@ public class App1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Test Application");
+		try {
+		System.out.println("----Test Application");
+		System.out.println("==========success===========");
 
 	}
+		catch(Exception e)
+		{
+			System.out.println("Error message");
+		}
 
+}
 }

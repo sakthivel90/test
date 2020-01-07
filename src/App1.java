3494d32dab7629d7e2c7070652f518c1c88e18c1
sakt2419 <sakt2419@CSLC10109.global.chainsys.com>
2020-01-07 15:36:34 +0530

    static block added

--- src/App1.java
+++ src/App1.java
@@ -1,9 +1,13 @@
 
 public class App1 {
 
+	static {
+		System.out.println("Started");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			
 		System.out.println("----Test Application1234444444");
 		System.out.println("==========success=========++++==");
 
